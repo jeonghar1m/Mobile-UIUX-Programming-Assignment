44a127e8fa901b4381a819bbe32d69f75a1e4115
Harim Jeong <harim97714@naver.com>
2021-12-16 01:05:12 +0900

    설정 페이지에 스위치 구현

--- Components/Screen/SettingPage/SettingPage.js
+++ Components/Screen/SettingPage/SettingPage.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Switch } from 'react-native';
 
 function SettingPage() {
-    return (
-      <View style={styles.container}>
-        <Text>SettingScreen</Text>
+  const [isDarkmodeEnabled, setIsDarkmodeEnabled] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.switch}>
+        <Text style={{marginRight: '1%', marginTop: '1.5%'}}>다크 모드</Text>
+        <Switch 
+          trackColor={{false: '#767577', true: '#81C147'}}
+          thumbColor={'#fff'}
+          ios_backgroundColor= '#3e3e3e'
+          onValueChange={() => setIsDarkmodeEnabled(!isDarkmodeEnabled)}
+          value={isDarkmodeEnabled}
+        />
       </View>
-    );
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  switch: {
+    flexDirection: 'row'
+  }
+});
 
 export default SettingPage;
